30e79a0ea58875d7cfcd7798e8959d26b81ec8da
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2025-09-03 16:38:49 +0300

    Solve the 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+    //Or (typeof numOne == 'number' && typeof numTwo == 'number') but includes the Double Numbers
+    let total = 0;
+    if ( numOne > 0 && numTwo > 0 && Number.isInteger(numOne) && Number.isInteger(numTwo)) {
+        let min = Math.min(numOne, numTwo);
+        let max = Math.max(numOne, numTwo);
+        for (let i = min; i <= max; i++) {
+            total += min;
+            min++;
+        }
+        return total;
+    } else {
+        return "ERROR";
+    }
 };
+
+let c = 2.5;
+
+console.log(typeof c);
+
 
 // Do not edit below this line
 module.exports = sumAll;
