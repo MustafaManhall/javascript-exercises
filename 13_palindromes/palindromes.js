afce444568377c3d58c7c9a4dbf524a195129a94
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2025-09-09 20:57:02 +0300

    Fix: solve 13_palindromes exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleaned = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    console.log(cleaned);
+    let reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
