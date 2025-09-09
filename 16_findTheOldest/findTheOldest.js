e9d0a86a384ed832b362cafe73a2d5ab24a7e05a
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2025-09-09 22:17:37 +0300

    Fix: solve 16_findTheOldest exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((pre, curr) => {
+        let preAge = getAge(pre.yearOfDeath, pre.yearOfBirth);
+        let currAge = getAge(
+            curr.yearOfDeath,
+            curr.yearOfBirth
+        );
+
+        return preAge < currAge ? curr : pre;
+    });
 };
 
 // Do not edit below this line
