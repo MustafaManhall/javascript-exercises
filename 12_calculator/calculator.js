6b0b36f55136400719004f3baa379eca6ed47783
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2025-09-09 20:30:29 +0300

    Fix: solve 12_calculator exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((pre,cur) => pre + cur,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((pre,cur) => pre * cur);
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	return Math.pow(num,pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+	for ( let i=1; i<=num; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
