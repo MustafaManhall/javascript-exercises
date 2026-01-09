4124e87fde1b605f982dccafb54e8ba1b0918df9
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2026-01-09 17:48:16 +0300

    Fix: Solve 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,17 @@
-const contains = function() {
-  
+const contains = function(obj, goal) {
+    for (let key in obj) {
+
+        let element = obj[key];
+        
+        if (element === goal) return true;
+
+        if (element !== element && goal !== goal) return true;
+         
+        if (typeof element === 'object' && element !== null) {
+            if (contains(element, goal)) return true;
+        }
+    }
+    return false;
 };
   
 // Do not edit below this line
